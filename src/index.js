9878a2073992c55584610cacbe034891ea94151f
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-21 18:15:38 -0400

    wire up reducers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import reducer from './store/reducer';
+import thunk from 'redux-thunk';
 
 // import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import filmCaseReducer from './store/reducers/filmCase';
+import orderReducer from './store/reducers/order';
 
 const store = createStore(reducer);
+
+const rootReducer = combineReducers({
+    filmCase: filmCaseReducer,
+    order: orderReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 const app = (
   <Provider store={store}>
