d981e07ae87b950af9405c1c7756e9f23fb13e41
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-21 17:50:15 -0400

    remove extra files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './store/reducer';
 
-import './index.css';
+// import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
