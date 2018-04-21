a9c0643d970e552b8e5b970cf911bae8b184f568
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-21 18:22:40 -0400

    add actions to filmcase

--- src/containers/FilmCase/FilmCase.js
+++ src/containers/FilmCase/FilmCase.js
@@ -8,45 +8,25 @@ import axios from '../../axios-orders';
 import Film from '../../components/Film/Film';
 import Controls from '../../components/Film/Controls/Controls';
 import OrderSummary from '../../components/Film/OrderSummary/OrderSummary';
-import * as actionTypes from '../../store/actions';
+import * as actions from '../../store/actions/index';
 
 import { Modal, Spin } from 'antd';
-
-// const FILM_PRICES = {
-//   panf: 4.5,
-//   delta100: 4.0,
-//   hp5: 5.5,
-//   delta3200: 8.0
-// };
 
 class FilmCase extends Component {
 
   state = {
-    purchasable: false,
     purchasing: false,
-    loading: false,
   }
 
   // state = {
-  //   films: null,
-  //   totalPrice: 0,
   //   purchasable: false,
   //   purchasing: false,
   //   loading: false,
   // }
 
+
   componentDidMount() {
-    // axios.get('https://ilfordfilm-61890.firebaseio.com/films.json')
-      // .then(response => {
-        // this.setState({
-          // films: response.data
-        // });
-      // })
-      // .catch(error => {
-        // this.setState({
-          // error: true
-        // });
-      // });
+    this.props.onInitFilms();
   }
 
   updatePurchaseState(films) {
@@ -58,47 +38,7 @@ class FilmCase extends Component {
         return sum + el;
       }, 0);
     return sum > 0;
-    // this.setState({
-      // purchasable: sum > 0
-    // });
   }
-
-  // addFilmHandler = (type) => {
-  //   const oldCount = this.state.films[type];
-  //   const updatedCount = oldCount + 1;
-  //   const updatedFilms = {
-  //     ...this.state.films
-  //   };
-  //   updatedFilms[type] = updatedCount;
-  //   const priceAddition = FILM_PRICES[type];
-  //   const oldPrice = this.state.totalPrice;
-  //   const newPrice = oldPrice + priceAddition;
-  //   this.setState({
-  //     totalPrice: newPrice,
-  //     films: updatedFilms
-  //   });
-  //   this.updatePurchaseState(updatedFilms);
-  // }
-
-  // removeFilmHandler = (type) => {
-  //   const oldCount = this.state.films[type];
-  //   if (oldCount <= 0) {
-  //     return;
-  //   }
-  //   const updatedCount = oldCount - 1;
-  //   const updatedFilms = {
-  //     ...this.state.films
-  //   };
-  //   updatedFilms[type] = updatedCount;
-  //   const priceDeduction = FILM_PRICES[type];
-  //   const oldPrice = this.state.totalPrice;
-  //   const newPrice = oldPrice - priceDeduction;
-  //   this.setState({
-  //     totalPrice: newPrice,
-  //     films: updatedFilms
-  //   });
-  //   this.updatePurchaseState(updatedFilms);
-  // }
 
   purchaseHandler = () => {
     this.setState({
@@ -113,28 +53,20 @@ class FilmCase extends Component {
   }
 
   purchaseContinueHandler = () => {
-      this.props.history.push('/checkout');
+    this.props.onInitPurchase();
+    this.props.history.push('/checkout');
   }
 
-  // purchaseContinueHandler = () => {
-  //   const queryParams = [];
-  //   for (let i in this.state.films) {
-  //     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this
-  //       .state.films[i]));
-  //   }
-  //   queryParams.push('price=' + this.state.totalPrice);
-  //   const queryString = queryParams.join('&');
-  //   this.props.history.push({
-  //     pathname: '/checkout',
-  //     search: '?' + queryString
-  //   });
-  // }
-
-
   render() {
+
     const disabledInfo = {
-      ...this.state.flms
+      ...this.props.flms
     };
+
+    // const disabledInfo = {
+    //   ...this.state.flms
+    // };
+
     for (let key in disabledInfo) {
       disabledInfo[key] = disabledInfo[key] <= 0
     }
@@ -164,32 +96,9 @@ class FilmCase extends Component {
               purchaseContinued={this.purchaseContinueHandler}/>;
     }
 
-
-    // if (this.state.films) {
-    //   film = (
-    //     <Aux>
-    //         <Film films={this.state.films} />
-    //         <Controls
-    //           filmAdded={this.addFilmHandler}
-    //           filmRemoved={this.removeFilmHandler}
-    //           disabled={disabledInfo}
-    //           purchasable={this.state.purchasable}
-    //           ordered={this.purchaseHandler}
-    //           price={this.state.totalPrice} />
-    //     </Aux>
-    //   );
-
-      // orderSummary =
-      //   <OrderSummary 
-      //         films={this.state.films}
-      //         price={this.state.totalPrice}
-      //         purchaseCancelled={this.purchaseCancelHandler}
-      //         purchaseContinued={this.purchaseContinueHandler}/>;
+    // if (this.state.loading) {
+      // orderSummary = <Spin />;
     // }
-
-    if (this.state.loading) {
-      orderSummary = <Spin />;
-    }
     return (
       <Aux>
           <Modal 
@@ -207,17 +116,19 @@ class FilmCase extends Component {
 
 const mapStateToProps = state => {
     return {
-        flms: state.films,
-        price: state.totalPrice
+        flms: state.filmCase.films,
+        price: state.filmCase.totalPrice,
+        error: state.filmCase.error
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFilmAdded: (flmName) => dispatch({type: actionTypes.ADD_FILM, filmName: flmName}),
-        onFilmRemoved: (flmName) => dispatch({type: actionTypes.REMOVE_FILM, filmName: flmName})
+        onFilmAdded: (flmName) => dispatch(actions.addFilm(flmName)),
+        onFilmRemoved: (flmName) => dispatch(actions.removeFilm(flmName)),
+        onInitFilms: () => dispatch(actions.initFilms()),
+        onInitPurchase: () => dispatch(actions.purchaseInit())
     }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler( FilmCase, axios ));
-// export default withErrorHandler(FilmCase, axios);
