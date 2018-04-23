90cd37934bf2d628b199f2852f6730ed244d873d
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-22 20:43:58 -0400

    update readme

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,12 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
+
+import banner from '../../assets/banner.jpg';
 
 class Home extends Component {
-
   render() {
     return (
       <div>
-        <img style={{ maxHeight: "70vh", maxWidth: "100%"}} src={'http://www.brunosbildverkstad.se/image/4980/ILFORD1_3.jpg'} alt="ilford"/><span>{this.props.name}</span>
+        <img style={{ maxHeight: "70vh", maxWidth: "100%"}} src={banner} alt="ilford" />
       </div>);
   }
 }
